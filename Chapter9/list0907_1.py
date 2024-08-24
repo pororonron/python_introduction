a181d5c6b65932d79d5f2eb20221d2bd5a6906f9
pororonron <pgaaa@outlook.jp>
2024-08-25 02:36:33 +0900

    Finish Chapter9-7

--- Chapter9/list0907_1.py
+++ Chapter9/list0907_1.py
@@ -16,41 +16,36 @@ def mouse_press(e):
     global mouse_c
     mouse_c = 1
 
-neko = [
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0]
-]
+neko = []
+for i in range(10):
+    neko.append([0, 0, 0, 0, 0, 0, 0, 0])
 
 def draw_neko():
     for y in range(10):
         for x in range(8):
-           if neko[y][x] > 0:
-               cvs.create_image(x*72+60, y*72+60, image=img_neko[neko[y][x]], tag="NEKO")
+            if neko[y][x] > 0:
+                cvs.create_image(x*72+60, y*72+60, image=img_neko[neko[y][x]], tag="NEKO")
 
-def drop_neko():
-    for y in range(8, -1, -1):
-        for x in range(8):
-            if neko[y][x] != 0 and neko[y+1][x] == 0:
-                neko[y+1][x] = neko[y][x]
-                neko[y][x] = 0
+def yoko_neko():
+    for y in range(10):
+        for x in range(1, 7):
+            if neko[y][x] > 0:
+                if neko[y][x-1] == neko[y][x] and neko[y][x+1] == neko[y][x]:
+                    neko[y][x-1] = 7
+                    neko[y][x] = 7
+                    neko[y][x+1] = 7
 
 def game_main():
     global cursor_x, cursor_y, mouse_c
-    drop_neko()
+    if 660 <= mouse_x and mouse_x < 840 and 100 <= mouse_y < 160 and mouse_c == 1:
+        mouse_c = 0
+        yoko_neko()
     if 24 <= mouse_x and mouse_x < 24+72*8 and 24 <= mouse_y and mouse_y < 24+72*10:
         cursor_x = int((mouse_x-24)/72)
         cursor_y = int((mouse_y-24)/72)
         if mouse_c == 1:
             mouse_c = 0
-            neko[cursor_y][cursor_x] = random.randint(1, 6)
+            neko[cursor_y][cursor_x] = random.randint(1, 2)
     cvs.delete("CURSOR")
     cvs.create_image(cursor_x*72+60, cursor_y*72+60, image=cursor, tag="CURSOR")
     cvs.delete("NEKO")
@@ -58,7 +53,7 @@ def game_main():
     root.after(100, game_main)
 
 root = tkinter.Tk()
-root.title("クリックしてネコを置く")
+root.title("横に3つ並んだか")
 root.resizable(False, False)
 root.bind("<Motion>", mouse_move)
 root.bind("<ButtonPress>", mouse_press)
@@ -79,5 +74,7 @@ img_neko = [
 ]
 
 cvs.create_image(456, 384, image=bg)
+cvs.create_rectangle(660, 100, 840, 160, fill="white")
+cvs.create_text(750, 130, text="テスト", fill="red", font=("Times New Roman", 30))
 game_main()
 root.mainloop()
