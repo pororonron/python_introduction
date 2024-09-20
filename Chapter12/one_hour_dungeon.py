6d3456b6e2b6d4dcec4324f32ac9eea27040def4
pororonron <pgaaa@outlook.jp>
2024-09-20 23:24:31 +0900

    Add variable declaration

---
+++ Chapter12/one_hour_dungeon.py
@@ -0,0 +1,97 @@
+import pygame
+import sys
+import random
+from pygame.locals import * 
+
+#色の定義
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+CYAN = (0, 255, 255)
+BLINK = [(224, 255, 255), (192, 240, 255), (128, 224, 255), (64, 192, 255), (128, 224, 255), (192, 240, 255)]
+
+#画像の読み込み
+imgTitle = pygame.image.load("image/title.png")
+imgWall = pygame.image.load("image/wall.png")
+imgWall2 = pygame.image.load("image/wall2.png")
+imgDark = pygame.image.load("image/dark.png")
+imgPara = pygame.image.load("image/parameter.png")
+imgBtlBG = pygame.image.load("image/btlbg.png")
+imgEnemy = pygame.image.load("image/enemy0.png")
+imgItem = [
+    pygame.image.load("image/potion.png"),
+    pygame.image.load("image/blaze_gem.png"),
+    pygame.image.load("image/spoiled.png"),
+    pygame.image.load("image/apple.png"),
+    pygame.image.load("image/meat.png")
+]
+imgFloor = [
+    pygame.image.load("image/floor.png"),
+    pygame.image.load("image/tbox.png"),
+    pygame.image.load("image/cocoon.png"),
+    pygame.image.load("image/stairs.png")
+]
+imgPlayer = [
+    pygame.image.load("image/mychr0.png"),
+    pygame.image.load("image/mychr1.png"),
+    pygame.image.load("image/mychr2.png"),
+    pygame.image.load("image/mychr3.png"),
+    pygame.image.load("image/mychr4.png"),
+    pygame.image.load("image/mychr5.png"),
+    pygame.image.load("image/mychr6.png"),
+    pygame.image.load("image/mychr7.png"),
+    pygame.image.load("image/mychr8.png")
+]
+imgEffect = [
+    pygame.image.load("image/effect_a.png"),
+    pygame.image.load("image/effect_b.png")
+]
+
+#変数の宣言
+speed = 1
+idx = 0
+tmr = 0
+floor = 0
+fl_max = 1
+welcome = 0
+
+pl_x = 0
+pl_y = 0
+pl_d = 0
+pl_a = 0
+pl_lifemax = 0
+pl_life = 0
+pl_str = 0
+food = 0
+potion = 0
+blazegem = 0
+treasure = 0
+
+emy_name = ""
+emy_lifemax = 0
+emy_life = 0
+emy_str = 0
+emy_x = 0
+emy_y = 0
+emy_step = 0
+emy_blink = 0
+
+dmg_eff = 0
+btl_cmd = 0
+
+COMMAND = ["[A]ttack", "[P]otion", "[B]laze gem", "[R]un"]
+TRE_NAME = ["Potion", "Blaze gem", "Food spoiled", "Food +20", "Food +100"]
+EMY_NAME = ["Green slime", "Red slime", "Axe beast", "Ogre", "Sword man", "Death hornet", "Signal slime", "Devil plant", "Twin killer", "Hell"]
+
+MAZE_W = 11
+MAZE_H = 9
+maze = []
+for y in range(MAZE_H):
+    maze.append([0]*MAZE_W)
+
+DUNGEON_W = MAZE_W*3
+DUNGEON_H = MAZE_H*3
+dungeon = []
+for y in range(DUNGEON_H):
+    dungeon.append([0]*DUNGEON_W)
+
